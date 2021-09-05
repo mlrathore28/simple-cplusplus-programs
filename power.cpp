219128662e2f981ed99e1cfea7801c9292eb4d27
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 22:55:46 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,25 @@
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int exponent;
+    float base, result = 1;
+
+    cout << "Enter base and exponent respectively:  ";
+    cin >> base >> exponent;
+
+    cout << base << "^" << exponent << " = ";
+
+    while (exponent != 0) {
+        result *= base;
+        --exponent;
+    }
+
+    cout << result;
+    
+    return 0;
+}
