1b6f5895c05da0e26f130e10076fb5f84489574a
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 22:54:06 +0530

    Create reverse-number.cpp

---
+++ reverse-number.cpp
@@ -0,0 +1,22 @@
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reversedNumber = 0, remainder;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    while(n != 0) {
+        remainder = n%10;
+        reversedNumber = reversedNumber*10 + remainder;
+        n /= 10;
+    }
+
+    cout << "Reversed Number = " << reversedNumber;
+
+    return 0;
+}
