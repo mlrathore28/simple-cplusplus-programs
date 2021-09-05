ba515bb652d56cdf9bebe9888d68b0a295062b23
Mohan Lal Rathore <90147813+mlrathore28@users.noreply.github.com>
2021-09-05 22:51:03 +0530

    Create find-factorial.cpp

---
+++ find-factorial.cpp
@@ -0,0 +1,24 @@
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long double factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+    }
+
+    return 0;
+}
